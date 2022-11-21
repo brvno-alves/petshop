1b0c4c587c8b43d936d8a7426c249843c89e8d82
Bruno R C Alves <bruno-rca@hotmail.com>
2022-11-21 15:43:01 -0300

    Criado classes Pet,Especie, Raça e seus respectivos Repositories.

--- src/main/java/com/bruno/petshop/utils/PopulaDados.java
+++ src/main/java/com/bruno/petshop/utils/PopulaDados.java
@@ -8,9 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.bruno.petshop.domain.Categoria;
+import com.bruno.petshop.domain.Especie;
+import com.bruno.petshop.domain.Pet;
 import com.bruno.petshop.domain.Produto;
+import com.bruno.petshop.domain.Raca;
 import com.bruno.petshop.repository.CategoriaRepository;
+import com.bruno.petshop.repository.EspecieRepository;
+import com.bruno.petshop.repository.PetRepository;
 import com.bruno.petshop.repository.ProdutoRepository;
+import com.bruno.petshop.repository.RacaRepository;
 
 @Component
 public class PopulaDados {
@@ -19,6 +25,15 @@ public class PopulaDados {
 	
 	@Autowired
 	ProdutoRepository produtoRepository;
+	
+	@Autowired
+	EspecieRepository especieRepository;
+	
+	@Autowired
+	RacaRepository racaRepository;
+	
+	@Autowired
+	PetRepository petRepository;
 	
 	@PostConstruct
 	public void cadastrar() {
@@ -49,5 +64,26 @@ public class PopulaDados {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		Especie esp1 = new Especie (null, "Cachorro");
+		Especie esp2 = new Especie (null, "Gato");
+		
+		Raca rac1 = new Raca (null, "Shitzu");
+		Raca rac2 = new Raca (null, "Chow-Chow");
+		Raca rac3 = new Raca (null, "Akita");
+		Raca rac4 = new Raca (null, "Husky Siberiano");
+		Raca rac5 = new Raca (null, "Persa");
+		Raca rac6 = new Raca (null, "Sphynx");
+		Raca rac7 = new Raca (null, "Siamês");
+		Raca rac8 = new Raca (null, "Maine Coom");
+		Raca rac9 = new Raca (null, "SRD");
+	
+		Pet pet1 = new Pet(null, "Chico", 4, esp2, rac9);
+		Pet pet2 = new Pet(null, "Mel Gibsa", 3, esp2, rac9);
+		Pet pet3 = new Pet(null, "Tigrão", 8, esp1, rac4);
+		
+		especieRepository.saveAll(Arrays.asList(esp1, esp2));
+		racaRepository.saveAll(Arrays.asList(rac1,rac2,rac3,rac4,rac5,rac6,rac7,rac8,rac9));
+		petRepository.saveAll(Arrays.asList(pet1,pet2,pet3));
 	}
 }
